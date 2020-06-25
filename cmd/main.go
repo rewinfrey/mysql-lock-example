0154b101a93a0acb74035173f2a7f48ef198abd0
Rick Winfrey <rewinfrey@github.com>
2020-06-24 22:26:53 -0700

    Not generating deadlocks

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"sync"
 	"time"
@@ -15,8 +14,9 @@ import (
 const (
 	delay           = 2 * time.Second
 	timeoutDuration = 1 * time.Second
+	afterDuration   = 1 * time.Second
 	noDelay         = 0 * time.Second
-	numWorkers      = 2
+	numWorkers      = 20
 )
 
 func log(id int, context string, val interface{}) {
@@ -26,25 +26,31 @@ func log(id int, context string, val interface{}) {
 func newGoRoutine(id int, db *gorm.DB, wg *sync.WaitGroup) {
 	go func() {
 		defer wg.Done()
+		hasLock := make(chan bool)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-		txOptions := sql.TxOptions{Isolation: 0, ReadOnly: false}
+		// _, err := db.DB().ExecContext(ctx, "SET TRANSACTION ISOLATION LEVEL READ COMMITTED")
+		// if err != nil {
+		// 	log(id, "SetTxIsolationLevel", err)
+		// 	return
+		// }
 
-		tx, err := db.DB().BeginTx(context.Background(), &txOptions)
+		// txOptions := sql.TxOptions{Isolation: 0, ReadOnly: false}
+		// tx, err := db.DB().BeginTx(ctx, &txOptions)
+		tx, err := db.DB().BeginTx(ctx, nil)
 		if err != nil {
 			log(id, "BeginTx", err)
 			return
 		}
 
-		hasLock := make(chan bool)
-
 		go func(hasLock chan bool) {
-			ctx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
-			defer cancel()
+			defer close(hasLock)
 
 			_, acquireErr := tx.ExecContext(ctx, "INSERT INTO example (id) VALUES(1)")
 			if acquireErr != nil {
 				log(id, "acquireErr", acquireErr)
-				hasLock <- false
+				// hasLock <- false
 				return
 			}
 
@@ -58,23 +64,35 @@ func newGoRoutine(id int, db *gorm.DB, wg *sync.WaitGroup) {
 
 				time.Sleep(delay)
 
-				err = tx.Rollback()
-				if err != nil {
-					log(id, "Rollback", err)
-					return
-				}
-				return
-			}
+			} 
 
 			err = tx.Rollback()
 			if err != nil {
 				log(id, "Rollback", err)
 			}
 
+			return
+
+		case <-time.After(afterDuration):
+			log(id, "Timeout", "duration met")
+			// err = tx.Rollback()
+			// if err!= nil {
+			// 	log(id, "Rollback", err)
+			// }
+
+			cancel()
+			time.Sleep(delay)
 			wg.Add(1)
 			newGoRoutine(id*10, db, wg)
-		}
+			return
 
+		case done := <-ctx.Done():
+			log(id, "Ctx Done", done)
+			// err = tx.Rollback()
+			// if err!= nil {
+			// 	log(id, "Rollback", err)
+			// }
+		}
 		return
 	}()
 }
